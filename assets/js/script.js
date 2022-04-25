2bdcec4d1b0d8261677159b695b7596f90f0510c
Diego Suarez <diegosuarez713@gmail.com>
2022-04-25 19:17:31 -0300

    arreglando login

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,20 @@
+//varibles
+
 const email = document.getElementById("email"); 
 const password = document.getElementById("password"); 
+const formlogin = document.getElementById("form-login"); 
+const warningEmail = document.getElementById("warning-email"); 
+const warningPassword = document.getElementById("warning-password"); 
+
+function ingresar(){
+
+    if((email.value =! "") && (password.value =! "")) {
+        window.location = "home.html"; 
+    }else{
+        alert("Debe completar los campos correo electronico y password con cualquier dato para ingresar")
+    }
+
+
+}
 
 
